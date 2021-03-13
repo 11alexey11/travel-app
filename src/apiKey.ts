5cb3163349bc3e67a69a10b0cd8f9f5d11d3f3f5
Alexey Yanvarev <yanvarev_2015@mail.ru>
2021-03-14 02:19:57 +0400

    feat: add widgets for countries

---
+++ src/apiKey.ts
@@ -0,0 +1,3 @@
+const key = '351d2714b99a8b9522929c7b8ea79b2f';
+
+export default key;
--- src/component/CountryInformation/CountryInformation.tsx
+++ src/component/CountryInformation/CountryInformation.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import 'swiper/swiper-bundle.min.css';
 import styles from './CountryInformation.module.css';
 import './swiper.css'
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import ReactPlayer from 'react-player';
+import { Map, Placemark, YMaps } from 'react-yandex-maps';
+import key from '../../apiKey';
 
 // install Swiper modules
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
@@ -13,6 +16,44 @@ interface CountryInformationProps {
 }
 
 const CountryInformation: React.FC<CountryInformationProps> = ({ country }) => {
+    const mapRef: any = useRef(null);
+    const [weather, setWeather]: any = useState([]);
+
+    useEffect(() => {
+        console.log('Я тут');
+        const weatherURL = `http://api.openweathermap.org/data/2.5/weather?id=${country.capitalIdWeather}&appid=${key}&units=metric`;
+        fetch(weatherURL)
+            .then(res => res.json())
+            .then(data => setWeather(data));
+    }, [country]);
+
+    const highlightCountry = (ymaps: any, country: any) => {
+        const objectManager = new ymaps.ObjectManager();
+        ymaps.borders.load('001', { lang: 'ru', quality: 1 })
+            .then((result: any) => {
+                const regions = result.features.reduce(function (acc: any, feature: any) {
+                    if (feature.properties.iso3166 === country.countryISO) {
+                        const iso = feature.properties.iso3166;
+                        feature.id = iso;
+
+                        feature.options = {
+                            strokeColor: '#000',
+                            strokeOpacity: 0.5,
+                        };
+                        acc[iso] = feature;
+                        return acc;
+                    }
+                    return acc;
+                }, {});
+                result.features = [];
+                for (var reg in regions) {
+                    result.features.push(regions[reg]);
+                }
+                objectManager.add(result);
+                mapRef.current.geoObjects.add(objectManager);
+            });
+    }
+
 
     return (
         <main className={styles.countryWrapper}>
@@ -52,17 +93,49 @@ const CountryInformation: React.FC<CountryInformationProps> = ({ country }) => {
                         }
                     </Swiper>
                 </div>
-                <div>
-
+                <div className={styles.videoMapContainer}>
+                    <div>
+                        <ReactPlayer controls url={`${country.videoUrl}`} />
+                    </div>
+                    <div className={styles.mapContainer}>
+                        <YMaps
+                            query={{
+                                lang: 'en_RU',
+                                ns: 'use-load-option',
+                                load: 'Map,Placemark,control.ZoomControl,control.FullscreenControl,control.GeolocationControl,control.TypeSelector,geoObject.addon.balloon',
+                            }}
+                        >
+                            <Map
+                                instanceRef={mapRef}
+                                defaultState={{
+                                    center: [3.731845, 8.755385],
+                                    zoom: 1,
+                                    controls: ['zoomControl', 'fullscreenControl', 'geolocationControl', 'typeSelector'],
+                                }}
+                                width='100%'
+                                height='100%'
+                                modules={["borders", "ObjectManager"]}
+                                onLoad={(ymaps: any) => highlightCountry(ymaps, country)}
+                            >
+                                <Placemark
+                                    defaultGeometry={country.capitalCoordinates}
+                                    properties={{
+                                        balloonContentBody: `${country.capital}`,
+                                    }}
+                                />
+                            </Map>
+                        </YMaps>
+                    </div>
                 </div>
-                <div>
-
-                </div>
-                <div>
-
-                </div>
-                <div>
-
+                <div className={styles.widgetsContainer}>
+                    <div className={styles.weatherContainer}>
+                        <h4>{country.capital}</h4>
+                        <div>{weather.weather ? weather.weather[0].description : null}</div>
+                        <img src={weather.weather ? `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png` : ''} alt='icon' />
+                        <div>{weather.weather ? Math.round(weather.main.temp) : null}°C</div>
+                    </div>
+                    <div></div>
+                    <div></div>
                 </div>
             </div>
         </main>
