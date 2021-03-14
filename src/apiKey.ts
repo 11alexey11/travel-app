13c5b4cff9a025020831bafed2136ab7aaf91165
Alexey Yanvarev <yanvarev_2015@mail.ru>
2021-03-14 14:44:06 +0400

    feat: add time and currency

--- src/apiKey.ts
+++ src/apiKey.ts
@@ -1,3 +1,2 @@
-const key = '351d2714b99a8b9522929c7b8ea79b2f';
-
-export default key;
+export const keyWeather = '351d2714b99a8b9522929c7b8ea79b2f';
+export const keyСurrency = '5bca3cf01e974582b14f601e7af836ca';
--- src/component/CountryInformation/CountryInformation.tsx
+++ src/component/CountryInformation/CountryInformation.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import 'swiper/swiper-bundle.min.css';
 import styles from './CountryInformation.module.css';
 import './swiper.css'
@@ -6,7 +6,7 @@ import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import ReactPlayer from 'react-player';
 import { Map, Placemark, YMaps } from 'react-yandex-maps';
-import key from '../../apiKey';
+import {keyWeather, keyСurrency} from '../../apiKey';
 
 // install Swiper modules
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
@@ -15,19 +15,45 @@ interface CountryInformationProps {
     country: any
 }
 
+const getTime = (timezone: number) => {
+    const today = new Date();
+    const localoffset = -(today.getTimezoneOffset() / 60);
+    const offset = timezone - localoffset;
+    const d = new Date(new Date().getTime() + offset * 3600 * 1000);
+    return [d.toLocaleDateString(), d.getHours(), d.getMinutes(), d.getSeconds()];
+}
+
 const CountryInformation: React.FC<CountryInformationProps> = ({ country }) => {
     const mapRef: any = useRef(null);
     const [weather, setWeather]: any = useState([]);
+    const [currency, setCurrency]: any = useState(0);
+    const [hours, setHours]: any = useState(getTime(country.timezone)[1]);
+    const [minutes, setMinutes]: any = useState(getTime(country.timezone)[2]);
+    const [seconds, setSeconds]: any = useState(getTime(country.timezone)[3]);
 
     useEffect(() => {
-        console.log('Я тут');
-        const weatherURL = `http://api.openweathermap.org/data/2.5/weather?id=${country.capitalIdWeather}&appid=${key}&units=metric`;
+        const weatherURL = `http://api.openweathermap.org/data/2.5/weather?id=${country.capitalIdWeather}&appid=${keyWeather}&units=metric&lang=${country.lang}`;
+        const currencyURL = `https://openexchangerates.org/api/latest.json?app_id=${keyСurrency}`;
         fetch(weatherURL)
             .then(res => res.json())
             .then(data => setWeather(data));
+
+        fetch(currencyURL)
+            .then(res => res.json())
+            .then(data => setCurrency(data.rates[country.currencyCode]));
     }, [country]);
 
-    const highlightCountry = (ymaps: any, country: any) => {
+    useEffect(() => {
+        const interval = setInterval(() => {
+            // setTime(time + 1);
+            setHours(getTime(country.timezone)[1]);
+            setMinutes(getTime(country.timezone)[2]);
+            setSeconds(getTime(country.timezone)[3]);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [country]);
+
+    const highlightCountry = useCallback((ymaps: any, country: any) => {
         const objectManager = new ymaps.ObjectManager();
         ymaps.borders.load('001', { lang: 'ru', quality: 1 })
             .then((result: any) => {
@@ -52,8 +78,7 @@ const CountryInformation: React.FC<CountryInformationProps> = ({ country }) => {
                 objectManager.add(result);
                 mapRef.current.geoObjects.add(objectManager);
             });
-    }
-
+    }, []);
 
     return (
         <main className={styles.countryWrapper}>
@@ -134,8 +159,14 @@ const CountryInformation: React.FC<CountryInformationProps> = ({ country }) => {
                         <img src={weather.weather ? `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png` : ''} alt='icon' />
                         <div>{weather.weather ? Math.round(weather.main.temp) : null}°C</div>
                     </div>
-                    <div></div>
-                    <div></div>
+                    <div className={styles.timeContainer}>
+                        <div className={styles.date}>{getTime(country.timezone)[0].toLocaleString('ca-IT')}</div>
+                        <div className={styles.watch}>{hours}:{Math.trunc(minutes / 10) === 0 ? `0${minutes}`: minutes}:{Math.trunc(seconds / 10) === 0 ? `0${seconds}`: seconds}</div>
+                    </div>
+                    <div className={styles.currencyContainer}>
+                        <h4>{country.currencyDescription}</h4>
+                        <div>{currency.toFixed(2)} $</div>
+                    </div>
                 </div>
             </div>
         </main>
