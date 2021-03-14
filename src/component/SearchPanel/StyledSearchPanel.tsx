74a67bfb1305bbb97848c206c9e5ffe05c30c760
alekseishkel <lexashkel@gmail.com>
2021-03-14 16:08:20 +0300

    feat: add search additional logic

---
+++ src/component/SearchPanel/StyledSearchPanel.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const StyledSearchPanel = styled.input`
+  {
+    width: 360px;
+    height: 50px;
+    padding: 0 20px;
+    padding-right: 60px;
+    background-color: transparent;
+    color: #fff;
+    font-size: 1rem;  
+    border: 2px solid #fff;
+    border-radius: 30px;
+    outline: none;
+  }
+`
+
+export default StyledSearchPanel;
